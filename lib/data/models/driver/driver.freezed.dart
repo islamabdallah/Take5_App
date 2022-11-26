a98f61124cfb7652dc6eedce682326f4efcfe6a7
asmaa <asmaasedeek97@gmail.com>
2022-11-26 08:42:02 +0200

    driver model

---
+++ lib/data/models/driver/driver.freezed.dart
@@ -0,0 +1,162 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'driver.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+Driver _$DriverFromJson(Map<String, dynamic> json) {
+  return _Driver.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Driver {
+  int? get id => throw _privateConstructorUsedError;
+  String? get fullName => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $DriverCopyWith<Driver> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $DriverCopyWith<$Res> {
+  factory $DriverCopyWith(Driver value, $Res Function(Driver) then) =
+      _$DriverCopyWithImpl<$Res, Driver>;
+  @useResult
+  $Res call({int? id, String? fullName});
+}
+
+/// @nodoc
+class _$DriverCopyWithImpl<$Res, $Val extends Driver>
+    implements $DriverCopyWith<$Res> {
+  _$DriverCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = freezed,
+    Object? fullName = freezed,
+  }) {
+    return _then(_value.copyWith(
+      id: freezed == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int?,
+      fullName: freezed == fullName
+          ? _value.fullName
+          : fullName // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_DriverCopyWith<$Res> implements $DriverCopyWith<$Res> {
+  factory _$$_DriverCopyWith(_$_Driver value, $Res Function(_$_Driver) then) =
+      __$$_DriverCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({int? id, String? fullName});
+}
+
+/// @nodoc
+class __$$_DriverCopyWithImpl<$Res>
+    extends _$DriverCopyWithImpl<$Res, _$_Driver>
+    implements _$$_DriverCopyWith<$Res> {
+  __$$_DriverCopyWithImpl(_$_Driver _value, $Res Function(_$_Driver) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = freezed,
+    Object? fullName = freezed,
+  }) {
+    return _then(_$_Driver(
+      id: freezed == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int?,
+      fullName: freezed == fullName
+          ? _value.fullName
+          : fullName // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_Driver implements _Driver {
+  const _$_Driver({this.id, this.fullName});
+
+  factory _$_Driver.fromJson(Map<String, dynamic> json) =>
+      _$$_DriverFromJson(json);
+
+  @override
+  final int? id;
+  @override
+  final String? fullName;
+
+  @override
+  String toString() {
+    return 'Driver(id: $id, fullName: $fullName)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_Driver &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.fullName, fullName) ||
+                other.fullName == fullName));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(runtimeType, id, fullName);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_DriverCopyWith<_$_Driver> get copyWith =>
+      __$$_DriverCopyWithImpl<_$_Driver>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_DriverToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Driver implements Driver {
+  const factory _Driver({final int? id, final String? fullName}) = _$_Driver;
+
+  factory _Driver.fromJson(Map<String, dynamic> json) = _$_Driver.fromJson;
+
+  @override
+  int? get id;
+  @override
+  String? get fullName;
+  @override
+  @JsonKey(ignore: true)
+  _$$_DriverCopyWith<_$_Driver> get copyWith =>
+      throw _privateConstructorUsedError;
+}
